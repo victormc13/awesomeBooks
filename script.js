ec17a6df0606aaa7e09000aad49f6d390fa79447
kambereBr <brunokambere@gmail.com>
2023-07-03 23:31:14 +0200

    Create book object, create addBook method and store data locally using javascript

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-let books = [
-  
-];
-
-function book(title, author) {
+function Book(title, author) {
   this.title = title;
   this.author = author;
 }
+
+let books = [];
+function addBook(title, author) {
+  const newBook = new Book(title, author);
+  books.push(newBook);
+  return books;
+}
+
+function storeBookLocally(bookArray) {
+  window.localStorage.setItem('bookArray', JSON.stringify(bookArray));
+}
+
+const addBtn = document.getElementById('add-btn');
+const titleField = document.getElementById('title');
+const authorField = document.getElementById('author');
+
+addBtn.addEventListener('click', function(){
+  addBook(titleField.value, authorField.value);
+  storeBookLocally(books)
+  const storedData = JSON.parse(localStorage.getItem('bookArray'));
+  titleField.value = '';
+  authorField.value = '';
+});
+
+
+
+
