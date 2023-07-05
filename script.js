52025f1e354a01aa3399824f9895c2e8468f6e07
victormc13 <viktorsb14@gmail.com>
2023-07-05 16:48:14 -0500

    Add the style and behavior of the sections and add current date

--- script.js
+++ script.js
@@ -97,27 +97,49 @@ function loadContent(id) {
   const bookCreation = document.querySelector('.addbook-section');
   const contact = document.querySelector('.contact-section');
 
+  const listMenu = document.getElementById('list-menu');
+  const addMenu = document.getElementById('add-menu');
+  const contactMenu = document.getElementById('contact-menu');
+
+
   if (id === 'list-menu') {
     bookList.style.display = 'block';
     bookCreation.style.display = 'none';
     contact.style.display = 'none';
+    listMenu.classList.toggle('active');
+    addMenu.classList.remove('active');
+    contactMenu.classList.remove('active');
   }
   if (id === 'add-menu') {
     bookList.style.display = 'none';
     bookCreation.style.display = 'block';
     contact.style.display = 'none';
+    listMenu.classList.remove('active');
+    addMenu.classList.toggle('active');
+    contactMenu.classList.remove('active');
   }
 
   if (id === 'contact-menu') {
     bookList.style.display = 'none';
     bookCreation.style.display = 'none';
     contact.style.display = 'block';
+    listMenu.classList.remove('active');
+    addMenu.classList.remove('active');
+    contactMenu.classList.toggle('active');
   }
 }
 
 const listMenu = document.getElementById('list-menu');
 const addMenu = document.getElementById('add-menu');
 const contactMenu = document.getElementById('contact-menu');
+
+setInterval(myTimer, 1000);
+
+function myTimer() {
+  const date = new Date().toUTCString();
+  document.querySelector(".date").innerHTML = date;
+}
+
 
 listMenu.addEventListener('click', function(){
   loadContent('list-menu');
